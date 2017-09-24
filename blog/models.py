45db73c5edbcadc1f5a03732455acd841b456645
reworth <reworth@163.com>
2017-09-25 00:47:38 +0800

    add Abstract func

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db  import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.html import strip_tags
+import markdown
 from django.utils.six import python_2_unicode_compatible
 
 class Category(models.Model):
@@ -33,6 +35,8 @@ class Post(models.Model):
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
     category = models.ForeignKey(Category)
+    #摘要
+    excerpt = models.CharField(max_length=200, blank=True)
     #阅读量统计
     views = models.PositiveIntegerField(default=0)
     tags = models.ManyToManyField(Tag, blank=True)
@@ -52,3 +56,17 @@ class Post(models.Model):
     def increase_views(self):
         self.views += 1
         self.save(update_fields=['views'])
+
+    def save(self, *args, **kwargs):
+        #如果没有填写摘要
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            #先将markdown文法渲染成html
+            #strip_tags去掉html文本的全部标签
+            #从文本摘取前54个字符赋值给excerpt
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post, self).save(*args, **kwargs)
+        
